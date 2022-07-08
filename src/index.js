c3583f7123bbbcb981efdd62e4dfd2475f9993b6
Dan Brima <brima.dan@gmail.com>
2022-07-08 08:39:08 +0300

    feat(entire project): add project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import { html, render } from 'https://unpkg.com/uhtml?module'
+// render(
+//   document.body,
+//   html`<h1 class="text-3xl font-bold underline">Hello 👋 µhtmlaaaaaaaaaaa</h1>`
+// )
 
-render(
-  document.body,
-  html`<h1 class="text-3xl font-bold underline">Hello 👋 µhtmlaaaaaaaaaaa</h1>`
-)
+const randomQuestionButtonId = 'random-question'
+const siteTextId = 'site-text'
+
+const animatedRandomQuestionButtonClasses =
+  'transition ease-in-out delay-150 -translate-y-80 hover:scale-110 duration-1000'
+const fadeOutClasses = 'transition-opacity ease-in duration-700 opacity-0'
+
+const showRandomQuestion = () => {
+  document.getElementById(siteTextId).className += fadeOutClasses
+
+  document.getElementById(
+    'random-question'
+  ).className += ` ${animatedRandomQuestionButtonClasses}`
+}
+
+document
+  .getElementById('random-question')
+  .addEventListener('click', showRandomQuestion)
